121e157935b2212d2b6afebe44d56860b34deb99
leaztanda <leaztanda@gmail.com>
2018-06-11 22:24:50 +0200

    quick integration

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <section id="home">
-    <section id="landing-page">
+    <section id="landing-page" class="scrollable">
       <img :src="require('./../assets/landing-background.png')" class="image-fullWidth" />
       <img :src="require('./../assets/landing-title.png')" class="image-title" />
     </section>
     <section id="concept-introduction">
       <div class="container-center">
-        <img :src="require('./../assets/logo.png')"  alt="" class="image-logo">
+        <img :src="require('./../assets/logo.png')"  alt="" class="image-logo scrollable scrollable-icon scrollable-absolute">
         <div class="scrollable scrollable-text">
           <h1>Une expérience interactive</h1>
           <p>Hodos est une <strong>expérience de lecture interactive</strong> sur tablette accompagnée d’un kit de cartes, destinée aux <strong>enfants de 7 à 9 ans avides d’aventures et de découvertes</strong>.</p>
@@ -15,20 +15,21 @@
       </div>
     </section>
     <section id="quote">
+      <img :src="require('./../assets/Background-introduction.png')"  alt="" class="image-fullWidth">
       <div class="container-center">
-        <div class="quote-illustration"></div>
-        <!-- <img :src="require('./../assets/bebe-hermes.png')" class="quote-illustration" /> -->
-        <div class="scrollable scrollable-text">
-          <q>Le soleil se levait à peine quand Hermès sortit du ventre de sa mère. Il s'étira, bâilla et sauta aussitôt sur ses pieds. Puis il courut à l'entrée de la grotte où il venait de naître pour admirer le monde. "Comme c'est beau !" murmura-t-il.</q>
+        <div class="container-left">
+          <div class="scrollable scrollable-text">
+            <h1>Une histoire extraordinaire</h1>
+            <p>Ce livre, animé du souffle des <strong>mythes fondateurs</strong>, nous convie à une lecture vagabonde : celle par laquelle nous suivons les <strong>mille et un rebondissements d'une histoire</strong> dont notre héros est le fil conducteur.</p>
+            <p>Mise à la portée de tous, cette histoire offre des réponses, souvent fabuleuses, aux questions que chaque être humain se pose.</p>
+          </div>
         </div>
-      </div>
-    </section>
-    <section id="concept-introduction">
-      <div class="container-center">
-        <div class="scrollable scrollable-text">
-          <h1>Une histoire extraordinaire</h1>
-          <p>Ce livre, animé du souffle des <strong>mythes fondateurs</strong>, nous convie à une lecture vagabonde : celle par laquelle nous suivons les <strong>mille et un rebondissements d'une histoire</strong> dont notre héros est le fil conducteur.</p>
-          <p>Mise à la portée de tous, cette histoire offre des réponses, souvent fabuleuses, aux questions que chaque être humain se pose.</p>
+        <div class="container-right">
+          <div class="quote-illustration"></div>
+          <!-- <img :src="require('./../assets/bebe-hermes.png')" class="quote-illustration" /> -->
+          <div class="scrollable scrollable-text">
+            <q>Le soleil se levait à peine quand Hermès sortit du ventre de sa mère. Il s'étira, bâilla et sauta aussitôt sur ses pieds. Puis il courut à l'entrée de la grotte où il venait de naître pour admirer le monde. "Comme c'est beau !" murmura-t-il.</q>
+          </div>
         </div>
       </div>
     </section>
@@ -36,7 +37,7 @@
       <img :src="require('./../assets/map-background.png')" class="image-fullWidth" />
       <div class="map-container">
         <div class="map-wrapper-left">
-          <img :src="require('./../assets/map-ipad.png')" class="image-ipad" />
+          <img :src="require('./../assets/map-ipad.png')" class="image-ipad scrollable scrollable-ipad" />
           <div class="container container-left">
             <div class="scrollable scrollable-text">
               <h1>Un plan évolutif</h1>
@@ -56,19 +57,25 @@
       <img :src="require('./../assets/cards-background.png')" class="image-fullWidth" />
       <img :src="require('./../assets/card-helios.png')" alt="" class="card-helios scrollable scrollable-card scrollable-absolute">
       <img :src="require('./../assets/card-aphrodite.png')" alt="" class="card-aphrodite scrollable scrollable-card scrollable-absolute">
+      <img :src="require('./../assets/card-zeus.png')" alt="" class="card-zeus scrollable scrollable-card scrollable-absolute">
+      <img :src="require('./../assets/card-artemis.png')" alt="" class="card-artemis scrollable scrollable-card scrollable-absolute">
     </section>
     <section id="story-introduction">
       <div class="container-center">
-        <div class="scrollable scrollable-text">
+        <div class="scrollable scrollable-text scrollable-absolute">
           <h1>Une bande défilée</h1>
           <p>Hodos est avant tout une <strong>expérience de lecture illustrée</strong> et prenant vie à travers une bande défilée divisée en 100 chapitres.</p>
         </div>
       </div>
-      <img :src="require('./../assets/story-ipad.png')" class="image-ipad" />
+      <!-- <img :src="require('./../assets/story-ipad.png')" class="image-ipad" /> -->
+      <div class="scrollable scrollable-ipad-story scrollable-absolute">
+        <img :src="require('./../assets/story-ipad-empty.png')" class="image-ipad" />
+        <img :src="require('./../assets/ipad-scrollable-story.png')" class="image-ipad-scrolled" />
+      </div>
     </section>
     <footer id="footer">
-      <img :src="require('./../assets/footer-background.png')" class="image-fullWidth desktopImage" />
-      <img :src="require('./../assets/footer-background-mobile.png')" class="image-fullWidth mobileImage" />
+      <img :src="require('./../assets/footer-background.png')" class="image-fullWidth desktopImage  clippedBackground" />
+      <img :src="require('./../assets/footer-background-mobile.png')" class="image-fullWidth mobileImage  clippedBackground" />
        <div class="container-center">
         <div class="container-wrapper">
           <a href="#">
@@ -114,9 +121,9 @@ export default {
         const offsetTopYPourcent = offsetTopY / window.innerHeight
 
         const offsetBottomYPourcent = offsetBottomY / window.innerHeight
-        if (offsetBottomYPourcent > 0.5 && offsetBottomYPourcent < 1) {
+        if (offsetBottomYPourcent > 0.5 && offsetBottomYPourcent < 1.5) {
           scrollable.classList.add('isScrolled')
-        } else if (scrollable.classList.contains('isScrolled')) {
+        } else if (scrollable.classList.contains('isScrolled') && (offsetBottomYPourcent < -1 || offsetBottomYPourcent > 3)) {
           scrollable.classList.remove('isScrolled')
         }
         // else if (!this.isScrollingUp && offsetTopYPourcent > 0 && offsetTopYPourcent < 1) {
@@ -147,10 +154,6 @@ export default {
 </script>
 
 <style lang="scss">
-// #lottie {
-//   width: 200px;
-//   height: 200px;
-// }
   .image {
     display: block;
     &-fullWidth {
@@ -178,31 +181,46 @@ export default {
 
     &-text {
       opacity: 0;
-      transform: translateY(-30px);
-      transition: transform .3s, opacity .5s;
+      transform: translateY(50px);
+      transition: transform .7s, opacity 1.5s;
       overflow: hidden;
     }
 
-    &::after {
-      // content: "";
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background: rgba(163, 253, 241, 0.596);
-      transform: translateY(100%);
-      transition: transform .5s;
+    &-ipad {
+      transform: translateY(30px) rotate(-5deg);
+      transition: transform .7s ease-in-out;
+    }
+
+    &-icon {
+      transform: translateY(-50px) scale(0.5);
+      opacity: 0;
+      transition: transform .5s, opacity .6s;
     }
 
     &.isScrolled {
       &.scrollable {
+        strong {
+          color: #4dc1f7;
+        }
         &-text {
           opacity: 1;
           transform: translateY(0);
+
         }
         &-card {
           transform: translateY(0) translateX(0) rotate(0) scale(1);
+        }
+        &-ipad {
+          transform: translateY(0) rotate(0);
+        }
+        &-ipad-story {
+          .image-ipad-scrolled {
+            transform: translateX(-280vw);
+          }
+        }
+        &-icon {
+          opacity: 1;
+          transform: translateY(0) scale(1);
         }
       }
       &::after {
@@ -231,11 +249,11 @@ export default {
   }
 
   h1 {
-    font-size: 28px;
+    font-size: 30px;
     font-weight: 400
   }
   p {
-    font-size: 13px;
+    font-size: 18px;
     line-height: 1.7;
   }
 
@@ -249,17 +267,71 @@ export default {
 
   strong {
     font-weight: 500;
-    color: #00194d;
+    transition: color .7s;
+    color: #333841;
+  }
+
+  #landing-page {
+    &.scrollable {
+      image {
+        width: 100%;
+        height: 50vw;
+        transform: translateY(-10vw);
+        transform: scale(1);
+      }
+      .image-title {
+        transform: scale(2) translateX(-25%);
+        transition: transform .5s;
+      }
+    }
+
+    &.isScrolled {
+      image {
+        transform: translateY(10vw) translateX(-50%);
+      }
+      .image-title {
+        transform: scale(1) translateX(-50%);
+      }
+    }
+
   }
 
   #quote {
-    margin-top: 80px;
+    margin-top: -10vw;
     text-align: center;
     .container-center {
       margin: 0 auto;
+      max-width: 80%;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+
+      .container-left,
+      .container-right {
+        max-width: 90%;
+      }
+
+      .container-right {
+        margin-top: -4vw;
+        text-align: right;
+        margin-bottom: 10vw;
+      }
+
+      .container-left {
+        margin-top: 10vw;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+      }
       q {
         font-size: 20px;
-        color: #4ca3ba
+        color: #4dc1f7;
+        &::before,
+        &::after {
+          font-size: 40px;
+        }
       }
     }
 
@@ -276,7 +348,7 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    margin-top: 100px;
+    margin-top: -10vw;
   }
 
   #map-introduction {
@@ -325,31 +397,82 @@ export default {
     &.card-helios {
       position: absolute;
       left: 5vw;
-      top: -5vw;
+      top: -8vw;
       width: 30vw;
+      max-width: 500px;
       height: auto;
-      transform: translateX(-150px) translateY(0) rotate(-15deg);
-      transition: transform .5s;
+      transform: translateX(-5vw) translateY(0) rotate(-15deg);
+      transition: transform 1s;
     }
 
     &.card-aphrodite {
       position: absolute;
       left: 30vw;
-      top: 10vw;
+      top: 15vw;
       width: 25vw;
+      max-width: 450px;
       height: auto;
-      transform: translateX(-180px) translateY(-30px) rotate(-5deg) scale(1.4);
-      transition: transform .5s;
+      transform: translateX(-15vw) translateY(-6vw) rotate(-10deg) scale(1.4);
+      transition: transform 1s;
+    }
+
+    &.card-zeus {
+      position: absolute;
+      left: 53vw;
+      top: 21vw;
+      width: 23vw;
+      max-width: 400px;
+      transform: translateX(-25vw) translateY(-5vw) rotate(-5deg) scale(1.7);
+      transition: transform 1s;
+    }
+
+    &.card-artemis {
+      position: absolute;
+      top: 21vw;
+      left: 80vw;
+      width: 15vw;
+      max-width: 300px;
+      transform: translateX(-25vw) translateY(-5vw) rotate(-12deg) scale(2.3);
+      transition: transform 1s;
     }
   }
+
+  .image-ipad-scrolled {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: auto;
+    z-index: 1;
+    transform: translateX(-10vw);
+    transition: transform 6s;
+  }
+
 
   #story-introduction {
     position: relative;
     z-index: 3;
-    .image-ipad {
-      margin: 0 auto;
-      max-width: 80vw;
+
+    .scrollable-ipad-story {
+      position: relative;
+      height: 45vw;
+      width: 58.8vw;
+      margin: 100px auto 50px;
+      overflow: hidden;
+      border-radius: 10Px;
     }
+
+    .image-ipad {
+      position: absolute;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      height: 100%;
+      width: 100%;
+      z-index: 2;
+    }
+
     .container {
       &-center {
         margin: 100px auto 50px;
@@ -419,9 +542,14 @@ export default {
     }
 
     .container {
-      max-width: 500px;
+      max-width: 550px;
       &-center {
-        max-width: 500px;
+        max-width: 550px;
+      }
+    }
+     #story-introduction {
+      .scrollable-ipad-story {
+        border-radius: 35Px;
       }
     }
     #map-introduction {
@@ -507,6 +635,25 @@ export default {
         }
         .map-wrapper-right {
           margin-top: 25vw;
+          text-align: left;
+        }
+      }
+    }
+
+     #quote {
+      .container-center {
+        justify-content: space-between;
+        flex-direction: row;
+
+        .container-left,
+        .container-right {
+          max-width: 40%;
+        }
+
+        .container-left {
+          margin-top: 30vw;
+        }
+        .container-right {
           text-align: left;
         }
       }
